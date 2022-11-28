723efbec06b5dc7f7389ee408d69ce88767af6e9
keemsora <itsyoursora@gmail.com>
2022-11-28 21:58:00 +0900

    [#11] ✨feat: Swagger 적용 및 기존 로직 수정

--- src/main/java/com/teamof4/mogu/controller/UserController.java
+++ src/main/java/com/teamof4/mogu/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.teamof4.mogu.controller;
 
-import com.teamof4.mogu.dto.UserDto;
 import com.teamof4.mogu.dto.UserDto.SaveRequest;
 import com.teamof4.mogu.service.UserService;
-import lombok.Getter;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +19,13 @@ import static com.teamof4.mogu.dto.UserDto.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
+@Api(tags = {"01. User API"})
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/create")
+    @ApiOperation(value = "회원 등록")
     public ResponseEntity<Void> createUser(@Valid @RequestPart SaveRequest requestDto,
                                            @RequestPart MultipartFile profileImage) {
         userService.save(requestDto, profileImage);
@@ -32,6 +34,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
+    @ApiOperation(value = "로그인")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest requestDto) {
 
         LoginResponse loginResponse = userService.login(requestDto);
--- src/main/java/com/teamof4/mogu/dto/PostDto.java
+++ src/main/java/com/teamof4/mogu/dto/PostDto.java
@@ -1,6 +1,8 @@
 package com.teamof4.mogu.dto;
 
 import com.teamof4.mogu.entity.*;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,23 +11,28 @@ import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class PostDTO {
+public class PostDto {
 
     @Getter
     @Builder
     public static class SaveRequest {
 
+        @ApiParam(value = "작성자 ID", required = true)
         private Long userId;
 
+        @ApiParam(value = "카테고리 ID", required = true)
         @NotBlank(message = "카테고리를 선택해 주세요.")
         private Long categoryId;
 
+        @ApiParam(value = "게시글 제목", required = true)
         @NotBlank(message = "제목을 입력해 주세요.")
         private String title;
 
+        @ApiParam(value = "게시글 내용", required = true)
         @NotBlank(message = "내용을 입력해 주세요.")
         private String content;
 
+        @ApiParam(value = "게시글 이미지 리스트")
         private List<MultipartFile> multipartFiles;
 
         public Post toEntity(User user, Category category) {
@@ -42,29 +49,53 @@ public class PostDTO {
     @Builder
     public static class UpdateRequest {
 
+        @ApiParam(value = "글 제목", required = true)
         @NotBlank(message = "제목을 입력해 주세요.")
         private String title;
 
+        @ApiParam(value = "글 내용", required = true)
         @NotBlank(message = "내용을 입력해 주세요.")
         private String content;
 
+        @ApiParam(value = "게시글 이미지 리스트")
         private List<MultipartFile> multipartFiles;
 
     }
 
     @Getter
     public static class Response {
+
+        @ApiModelProperty(notes = "게시글 ID")
         private Long id;
+
+        @ApiModelProperty(notes = "작성자 ID")
         private Long userId;
+
+        @ApiModelProperty(notes = "카테고리 ID")
         private Long categoryId;
+
+        @ApiModelProperty(notes = "작성자 닉네임")
         private String userNickname;
+
+        @ApiModelProperty(notes = "카테고리 이름")
         private String categoryName;
+
+        @ApiModelProperty(notes = "게시글 내용")
         private String title;
+
+        @ApiModelProperty(notes = "게시글 내용")
         private String content;
+
+        @ApiModelProperty(notes = "조회수")
         private int view;
 
+        @ApiModelProperty(notes = "게시글 이미지 리스트")
         private List<Image> imageList;
+
+        @ApiModelProperty(notes = "게시글 생성일")
         private LocalDateTime createAt;
+
+        @ApiModelProperty(notes = "게시글 수정일")
         private LocalDateTime updateAt;
 
         @Builder
@@ -78,32 +109,6 @@ public class PostDTO {
             this.content = post.getContent();
             this.view = post.getView();
             this.imageList = images;
-            this.createAt = post.getCreatedAt();
-            this.updateAt = post.getUpdatedAt();
-        }
-    }
-
-    @Getter
-    public static class ListResponse {
-        private Long id;
-        private Long userId;
-        private Long categoryId;
-        private String userNickname;
-        private String categoryName;
-        private String title;
-        private int view;
-        private LocalDateTime createAt;
-        private LocalDateTime updateAt;
-
-        @Builder
-        public ListResponse(Post post) {
-            this.id = post.getId();
-            this.userId = post.getUser().getId();
-            this.categoryId = post.getCategory().getId();
-            this.userNickname = post.getUser().getNickname();
-            this.categoryName = post.getCategory().getCategoryName();
-            this.title = post.getTitle();
-            this.view = post.getView();
             this.createAt = post.getCreatedAt();
             this.updateAt = post.getUpdatedAt();
         }
