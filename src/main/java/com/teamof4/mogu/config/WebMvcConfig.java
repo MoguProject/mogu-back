7021f6a278f8fffa1768d5af74ef9df3bc1a85de
frost0807 <junesuck99@naver.comgit config --listgit>
2022-12-14 02:27:04 +0900

    :wrench: config: cors url 추가

--- src/main/java/com/teamof4/mogu/config/WebMvcConfig.java
+++ src/main/java/com/teamof4/mogu/config/WebMvcConfig.java
@@ -13,7 +13,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true)
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://182.218.50.35", "http://182.218.50.35:41688", "http://182.218.50.35:3000",
+                        "182.218.50.35", "182.218.50.35:41688", "182.218.50.35:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .maxAge(MAX_AGE_SECS);
